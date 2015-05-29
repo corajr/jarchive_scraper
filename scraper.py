d01602359e78e9fb282ea1b65760594368c06b87
Cora Johnson-Roberson <cora.johnson.roberson@gmail.com>
2015-05-29 14:34:36 -0400

    remove limit on seasons

--- scraper.py
+++ scraper.py
@@ -12,7 +12,7 @@ def scrape_all_seasons(url):
     soup = BeautifulSoup(scraperwiki.scrape(url))
     
     #Grab all of the seasons listed
-    seasons = soup.find('div', {"id":"content"}).find_all('a', limit=3)
+    seasons = soup.find('div', {"id":"content"}).find_all('a')
     for season in seasons:
         scrape_season(base_url+season['href'])
 
